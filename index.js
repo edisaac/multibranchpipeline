d62da6f9840e965755f8f69ebc4c749fe73b8865
Edilberto Mejia Flores <edilberto.mejia@laureate.pe>
2024-05-16 15:20:48 -0500

    version1

---
+++ index.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+    // Obtener los valores de las variables de entorno
+    const apiKeyLambda = process.env.API_KEY_LAMBDA;
+    const modalidades = process.env.MODALIDADES;
+    const apiKeyGestorNotificaciones = process.env.API_KEY_GESTOR_NOTIFICACIONES;
+    
+    // Construir la respuesta
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            apiKeyLambda: apiKeyLambda,
+            modalidades: modalidades,
+            apiKeyGestorNotificaciones: apiKeyGestorNotificaciones
+        })
+    };
+    
+    return response;
+};
